fcca93103c5d5083d155244bbffca12a4e6fa85e
Benoit Pierre <benoit.pierre@gmail.com>
2021-05-03 20:07:51 +0200

    improve tests

--- check.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-from importlib.util import find_spec
-import ctypes
-
-spec = find_spec('pyusb_libusb1_backend.libusb')
-assert spec is not None
-libusb = ctypes.CDLL(spec.origin)
